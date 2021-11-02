3db899af1974de9ea8fa45bf5f0415819f06187c
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-11-02 13:05:12 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstCard = 8
+let secondCard = 9
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log ("Do you want to draw another card? ")
+
+} else if (sum===21) {
+    console.log
+}
