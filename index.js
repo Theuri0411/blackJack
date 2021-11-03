a02d73dff5b686c9c358c85ddbdc17d52286d85f
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-11-03 22:15:42 +0300

    black jack update

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let firstCard = 8
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
+isAlive = true
+let message = ""
 
-if (sum < 21) {
-    console.log ("Do you want to draw another card? ")
 
-} else if (sum===21) {
-    console.log
+if (sum <= 20) {
+   message = "Do You Want To Draw Another Card? "
+} else if (sum === 21) {
+    message = "You've Got Blackjack"
+    hasBlackJack = true
+} else {
+    message = "You are out of the game" 
+    isAlive = false
 }
+
+console.log (message)
+
